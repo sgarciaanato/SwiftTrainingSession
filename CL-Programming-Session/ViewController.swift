c5ac2e10f08f06f00da9a01deccea8a532d60b9e
s0g0l52 <samuel.garcia@walmart.com>
2024-11-12 23:44:04 -0300

    Initial Commit

---
+++ CL-Programming-Session/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CL-Programming-Session
+//
+//  Created by Samuel Garcia on 12-11-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
